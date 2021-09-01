317d3f7d1885b515e7d6c8541c99f2c539bfa640
Lisiralei <realalleerrr1@gmail.com>
2021-09-01 20:43:34 +0500

    Security issue remediated

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from keychain import djangosupersecretkey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%f1p!ne6l4%cp#qr2-33&0&gl10wyic$)8f)x78d-9kgj*ns='
+SECRET_KEY = djangosupersecretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
